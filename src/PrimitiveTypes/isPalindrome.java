6380361a8ea2f591fa38ef364c61a6800cf3acf7
Anitha Boppagowdanapura Shivalingaswamy <anithabs.ec@gmail.com>
2018-06-03 18:04:35 -0500

    6/2

---
+++ src/PrimitiveTypes/isPalindrome.java
@@ -0,0 +1,10 @@
+package PrimitiveTypes;
+
+public class isPalindrome {
+	public static void main(String args[]){
+		int x = 157751;
+		final int numOfDigits = (int)Math.floor(Math.log10(x)) + 1;
+		System.out.println(numOfDigits);
+	}
+
+}
